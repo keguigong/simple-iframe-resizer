0da9ccd3578fa8b39f8ced19e770fb1e78717d98
keguigong <keguigong@gmail.com>
2025-01-12 21:29:49 +0800

    chore: add support for tailwindcss

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginLess } from '@rsbuild/plugin-less';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(), //
+    pluginLess(),
+  ],
+  tools: {
+    postcss: {
+      postcssOptions: {
+        plugins: [tailwindcss],
+      },
+    },
+  },
 });
