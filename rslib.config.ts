9921f1eb618d82c7a924a72a7c69b6af4781c929
keguigong <keguigong@gmail.com>
2025-02-08 17:43:03 +0800

    feat: can successfully build output with rslib

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from '@rsbuild/core';
+import { defineConfig } from '@rslib/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginLess } from '@rsbuild/plugin-less';
 import tailwindcss from 'tailwindcss';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
+  source: {
+    entry: {
+      index: ['./src/index.ts'],
+    },
+
+    define: {
+      __DEV__: isDev,
+    },
+  },
+  lib: [
+    {
+      bundle: true,
+      dts: true,
+      format: 'esm',
+    },
+  ],
+  output: {
+    target: 'web',
+  },
   plugins: [
     pluginReact(), //
     pluginLess(),
